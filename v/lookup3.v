05a342f8269b4f6f0e7d381065e42949e548769c
sora <sora@wide.ad.jp>
2012-03-19 15:12:49 +0900

    rewriting lookup3.v

--- v/lookup3.v
+++ v/lookup3.v
@@ -1,133 +1,133 @@
-module main(out);
-	parameter STEP = 1000;
+`define STEP 2
+`define dbg  1
 
-	output	[31:0] out;
+module sim();
+reg        tb_enable = 1'b0;
+reg[639:0] tb_key;
 
-	reg	[80*8*1:1] key;
-	reg	[31:0] length, interval, k0, k1, k2;
-	reg	[31:0] a, b, c;
-	reg	[1:0] clk, res;
+wire CLK;
+wire RST;
+wire[31:0] tb_hashkey;
 
-	initial begin
-		$dumpfile("test.vcd");
-		$dumpvars(0,main);
-		// stats cachedump 1 100
-		// ITEM abcdefghijkl [3 b; 1324188529 s]
-		clk = 1'b0;
-		res = 1'b0;
-		key = "abcdefghijkl";
-		length = 32'd12;
-		interval = 32'd0;
-	end
+clock clock (
+  .CLK(CLK),
+  .RST(RST)
+);
 
-	always #(STEP/2) clk = ~clk;
+mix mix (
+  .CLK(CLK),
+  .RST(RST),
+  .enable(tb_enable),
+  .key(tb_key),
+  .hashkey(tb_hashkey)
+);
 
-	always @(posedge clk) begin
-		a = 32'hDEADBEEF + length + interval;
-		b = 32'hDEADBEEF + length + interval;
-		c = 32'hDEADBEEF + length + interval;
-		$display("1: a=%h; b=%h; c=%h", a, b, c);
+initial begin
+  //$monitor("state: %d", lookup2.state);
+  #100 tb_enable = 1'b1; tb_key = "abcdefghijkl";
+  #100 tb_enable = 1'b1; tb_key = "abcdefghijkl";
+  #100 tb_enable = 1'b1; tb_key = "abcdefghijkl";
+  #100 $finish;
+end
 
-		k0 = key[3*4*8+1:2*4*8+1];
-		k1 = key[2*4*8+1:1*4*8+1];
-		k2 = key[1*4*8+1:1];
+initial begin
+  $dumpfile("mix.vcd");
+  $dumpvars(0, sim.mix);
+end
 
-		/* DEBUG: convert big endian into little endian */
-		k0 = { k0[7:0], k0[15:8], k0[23:16], k0[31:24] };
-		k1 = { k1[7:0], k1[15:8], k1[23:16], k1[31:24] };
-		k2 = { k2[7:0], k2[15:8], k2[23:16], k2[31:24] };
-		/* DEBUG: end */
+always @(posedge CLK) begin
+  if (RST)
+    tb_enable <= 1'b0;
+  else begin
+    if (tb_hashkey)
+      $display("key: %s, hashkey: %h", tb_key, tb_hashkey);
+      $display("%x, %x, %x", tb_key[95:64], tb_key[63:32], tb_key[31:0]);
+    if (tb_enable)
+      tb_enable <= 1'b0;
+  end
+end
 
-		$display("k: k0=%h; k1=%h; k2=%h", k0, k1, k2);
+endmodule // sim()
 
-		case (length)
-			32'd12: begin
-				$display("c=%h; k2=%h", c, k2);
-				c <= c + k2;
-				$display("c=%h; k2=%h", c, k2);
-				b <= b + k1;
-				a <= a + k0;
-			end
-			32'd11: begin
-				c <= c + k2 & 32'hFFFFFF00;
-				b <= b + k1;
-				a <= a + k0;
-			end
-			32'd10: begin
-				c = c + k2 & 32'hFFFF0000;
-				b = b + k1;
-				a = a + k0;
-			end
-			32'd9: begin
-				c = c + k2 & 32'hFF000000;
-				b = b + k1;
-				a = a + k0;
-			end
-			32'd8: begin
-				b = b + k1;
-				a = a + k0;
-			end
-			32'd7: begin
-				b = b + k1 & 32'hFFFFFF00;
-				a = a + k0;
-			end
-			32'd6: begin
-				b <= b + k1 & 32'hFFFF0000;
-				a <= a + k0;
-			end
-			32'd5: begin
-				b <= b + k1 & 32'hFF000000;
-				a <= a + k0;
-			end
-			32'd4: begin
-				a <= a + k0;
-			end
-			32'd3: begin
-				a <= a + k0 & 32'hFFFFFF00;
-			end
-			32'd2: begin
-				a <= a + k0 & 32'hFFFF0000;
-			end
-			32'd1: begin
-				a <= a + k0 & 32'hFF000000;
-			end
-			32'd0: begin
-				$display("!!OUT=%h", c);
-				$finish;
-			end
-			default: begin
-				$display("ERROR");
-				$finish;
-			end
-		endcase
+module clock (
+  output reg CLK,
+  output reg RST
+);
 
-		$display("2: a=%h; b=%h; c=%h", a, b, c);
-
-		/* final */
-		c = c ^ b; c = c - ((b << 14) ^ (b >> (32 - 14)));
-		a = a ^ c; a = a - ((c << 11) ^ (c >> (32 - 11)));
-		b = b ^ a; b = b - ((a << 25) ^ (a >> (32 - 25)));
-		c = c ^ b; c = c - ((b << 16) ^ (b >> (32 - 16)));
-		a = a ^ c; a = a - ((c << 4) ^ (c >> (32 - 4)));
-		b = b ^ a; b = b - ((a << 14) ^ (a >> (32 - 14)));
-		c = c ^ b; c = c - ((b << 24) ^ (b >> (32 - 24)));
-		$display("3: a=%h; b=%h; c=%h", a, b, c);
-		$display("!!OUT=%X", c);
-		$finish;
-	end
+initial CLK = 0;
+initial RST = 0;
+always #(`STEP / 2) begin
+  CLK <= ~CLK;
+  RST <= 0;
+end
 endmodule
 
-//module rotate1(x, k, o);
-//	input  [31:0] x, k;
-//	output [31:0] o;
-//
-//	assign o = (x << k) ^ (x >> (32 - k));
-//endmodule
-//
-//module mix(ia, ib, ic, oa, ob, oc);
-//	input  [31:0] ia, ib, ic;
-//	output [31:0] oa, ob, oc;
-//
-//	wire [31:0] ia, ib, ic, oa, ob, oc;
-//endmodule
-//
+module mix (
+  input  CLK,
+  input  RST,
+  input  enable,
+  input  [639:0] key,
+  output wire[31:0] hashkey
+);
+
+parameter[1:0] s0 = 2'b00, //idle
+               s1 = 2'b01;
+parameter length   = 0;
+parameter interval = 0;
+
+reg[2:0]   state;
+reg[31:0]  a0, b0, c0, a1, b1, c1, a2, b2, c2,
+           a3, b3, c3, a4, b4, c4;
+wire[31:0] next_a1, next_c1, next_a2, next_c2,
+           next_a3, next_c3, next_a4, next_c4;
+
+wire[31:0] k0      = key[95:64];
+wire[31:0] k1      = key[63:32];
+wire[31:0] k2      = key[31:0];
+wire[31:0] next_a0 = 32'hDEADBEEF + length + interval;
+wire[31:0] next_b0 = 32'hDEADBEEF + length + interval;
+wire[31:0] next_c0 = 32'hDEADBEEF + length + interval;
+
+assign next_a1 = (a0 - c0) ^ {c0[27:0], c0[31:28]};
+assign next_c1 = c0 + b0;
+assign next_a2 = a1 + c1;
+assign next_c2 = (c1 - b1) ^ {b1[23:0], b1[31:24]};
+assign next_a3 = (a2 - c2) ^ {c2[15:0], c2[31:16]};
+assign next_c3 = c2 + b2;
+assign next_a4 = a3 + c3;
+assign next_c4 = (c3 - b3) ^ {b3[27:0], b3[31:28]};
+
+assign hashkey = c4;
+
+always @(posedge CLK) begin
+  if (RST)
+    state <= s0;
+  else begin
+    a0 <= next_a0 + k0;
+    b0 <= next_b0 + k1;
+    c0 <= next_c0 + k2;
+    a1 <= next_a1;
+    c1 <= next_c1;
+    b1 <= (b0 - next_a1) ^ {next_a1[25:0], next_a1[31:26]};
+    a2 <= next_a2;
+    c2 <= next_c2;
+    b2 <= b1 + next_a2;
+    a3 <= next_a3;
+    c3 <= next_c3;
+    b3 <= (b2 - next_a3) ^ {next_a3[12:0], next_a3[31:13]};
+    a4 <= next_a4;
+    c4 <= next_c4;
+    b4 <= b3 + next_a4;
+
+    state <= s0;
+  end
+end
+
+always @*
+  if (enable)
+    state <= s1;
+
+endmodule
+
+module final (
+
